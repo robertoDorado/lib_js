0b91680f16d2eaa8df140d82e17a24a5b4941f46
roberto_dorado <roberto.dorado@anexxa.com.br>
2021-12-03 17:59:18 -0300

    lib js em construção

---
+++ index.js
@@ -0,0 +1,82 @@
+class HelpersJs {
+
+    constructor(url='') {
+        this.url = url
+    }
+
+    selectElement = (elem) => document.querySelector(elem)
+    selectElements = (elems) => document.querySelectorAll(elems)
+
+    listener = (listen, element, functionName) => {
+        //Se o listener for submit ele previne a ação padrão
+        if(listen == 'submit'){
+            this.selectElement(element).addEventListener(listen, (e) => {
+                e.preventDefault()
+            })
+        }
+
+        return this.selectElement(element).addEventListener(listen, functionName)
+    }
+
+    readyState = (xhr, functionName) => xhr.addEventListener('readystatechange', functionName)
+    parseJSON = (string) => JSON.parse(string)
+    stringJSON = (object) => JSON.stringify(object)
+
+    visible = (element) => this.selectElement(element).style.display = 'block'
+    invisible = (element) => this.selectElement(element).style.display = 'none'
+
+    requestXHR(format, data, method) {
+        
+        let xhr = new XMLHttpRequest()
+        let form_data = new FormData()
+
+        if(method == 'POST'){
+
+            switch (format) {
+                case 'json':
+                    data = this.stringJSON(data)
+                    break
+    
+                case 'querystring':
+                    let query_string = []
+                    for (let i in data) {
+                        if (query_string.indexOf(`${i}=${data[i]}`) == -1) {
+                            query_string.push(`${i}=${data[i]}`)
+                        }
+                    }
+                    data = query_string.join('&')
+                    break
+            }
+    
+            form_data.append('data', data)
+    
+            xhr.open('POST', this.url)
+            xhr.send(form_data)
+        }
+
+        if(method == 'GET'){
+            this.readyState(xhr, () => {
+                return xhr.response
+            })
+        }
+
+        return xhr
+    }
+
+
+    popup(elem, altura, largura) {
+
+        this.selectElement(elem).addEventListener('click', (e) => {
+            e.preventDefault()
+            let w = screen.width;
+            let h = screen.height;
+            let meio_w = w / 2;
+            let meio_h = h / 2;
+            let altura2 = altura / 2;
+            let largura2 = largura / 2;
+            let meio1 = meio_h - altura2;
+            let meio2 = meio_w - largura2;
+            return window.open(this.url, '', `scrollbars=no,toolbar=no,location=no,directories=no,status=no,menubar=no,resizable=no,copyhistory=no,height=${altura},width=${largura},top=${meio1},left=${meio2}`)
+        })
+    }
+}
